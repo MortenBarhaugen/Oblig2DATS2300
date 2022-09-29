3504bf4df8e04be8e2de230d98d882207c6e152a
MortenBarhaugen <42533141+MortenBarhaugen@users.noreply.github.com>
2022-09-29 14:52:57 +0200

    Laget et utkast av konstruktør, antall(), tom() og omvendtString(). Laget klasser for å teste oppgave 1 og 2.

--- algdat-2022-oblig-2-master/src/main/java/no/oslomet/cs/algdat/Oblig2/sjekkOppgave1.java
+++ algdat-2022-oblig-2-master/src/main/java/no/oslomet/cs/algdat/Oblig2/sjekkOppgave1.java
@@ -2,7 +2,12 @@ package no.oslomet.cs.algdat.Oblig2;
 
 public class sjekkOppgave1 {
     public static void main(String[] args) {
-        Liste<String> liste = new DobbeltLenketListe<>();
+        String[] s1 = {"D",null,"E","F","G", null};
+        DobbeltLenketListe<String> liste = new DobbeltLenketListe<>(s1);
         System.out.println(liste.antall() + " " + liste.tom());
+
+        String[] s3 = {};
+        DobbeltLenketListe<String> liste3 = new DobbeltLenketListe<>(s3);
+        System.out.println(liste3.antall() + " " + liste3.tom());
     }
 }
