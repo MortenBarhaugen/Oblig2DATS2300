8227666f4e945a0b3699cccb131879eb0b879e81
MortenBarhaugen <42533141+MortenBarhaugen@users.noreply.github.com>
2022-10-12 14:39:29 +0200

    Fjernet kommentar i sjekk-fil for oppgave 1

--- src/main/java/no/oslomet/cs/algdat/Oblig2/sjekkOppgave1.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/sjekkOppgave1.java
@@ -4,14 +4,6 @@ import java.util.Arrays;
 
 public class sjekkOppgave1 {
     public static void main(String[] args) {
-        /*String[] s1 = {"D",null,"E","F","G", null};
-        DobbeltLenketListe<String> liste = new DobbeltLenketListe<>(s1);
-        System.out.println(liste.antall() + " " + liste.tom());
-
-        String[] s3 = {};
-        DobbeltLenketListe<String> liste3 = new DobbeltLenketListe<>(s3);
-        System.out.println(liste3.antall() + " " + liste3.tom());*/
-
         int antallFeil = 0;
         Liste<Integer> liste = new DobbeltLenketListe<>();
 
