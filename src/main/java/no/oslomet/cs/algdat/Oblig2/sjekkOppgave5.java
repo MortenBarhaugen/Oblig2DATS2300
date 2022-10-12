8e9f63ff926dd93b08fc1bae75106a92dd1b38a3
MortenBarhaugen <42533141+MortenBarhaugen@users.noreply.github.com>
2022-10-12 14:40:39 +0200

    Slettet kommentar i sjekk-fil for oppgave 5

--- src/main/java/no/oslomet/cs/algdat/Oblig2/sjekkOppgave5.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/sjekkOppgave5.java
@@ -4,24 +4,6 @@ import java.util.Arrays;
 
 public class sjekkOppgave5 {
     public static void main(String[] args) {
-        /*String[] s1 = {"D",null,"E","F","G", null};
-        DobbeltLenketListe<String> liste = new DobbeltLenketListe<>(s1);
-        System.out.println(liste.antall() + " " + liste.tom());
-
-        String[] s2 = {"A","B","C","D","E"};
-        DobbeltLenketListe<String> liste2 = new DobbeltLenketListe<>(s2);
-        System.out.println(liste2.antall() + " " + liste2.tom());
-        System.out.println(liste2.toString());
-        System.out.println(liste2.omvendtString());
-
-        String[] s3 = {};
-        DobbeltLenketListe<String> liste3 = new DobbeltLenketListe<>(s3);
-        System.out.println(liste3.antall() + " " + liste3.tom());
-
-        String[] s4 = {null, "A", "B", null, "C", null};
-        DobbeltLenketListe<String> liste4 = new DobbeltLenketListe<>(s4);
-        System.out.println(liste4.antall() + " " + liste4.tom());*/
-
         int antallFeil = 0;
         Liste<Integer> liste = new DobbeltLenketListe<>();
 
